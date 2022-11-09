a8b80dace6ec48fa48fc13449c0787c333bd3445
agshaathalla <agshaathalla@gmail.com>
2022-11-09 20:52:19 +0700

    Add fungsi dasar penjumlahan matriks and delete tes.txt

---
+++ src/operation.py
@@ -0,0 +1,11 @@
+import numpy as np
+import cv2
+
+# matriks[0] kolom
+
+def JumlahMatriks (matriksGambar1, matriksGambar2):
+    for i in range(len(matriksGambar1)):
+        for j in range(len(matriksGambar1[0])):
+            matriksGambar1[i][j] += matriksGambar2[i][j]
+    return matriksGambar1
+
