84e8c2acec56408296edd38173c4a9b2b6625f9d
agshaathalla <agshaathalla@gmail.com>
2022-11-09 21:06:15 +0700

    Add fungsi perkalian matriks

--- src/operation.py
+++ src/operation.py
@@ -14,3 +14,7 @@ def KurangMatriks (matriksGambar1, matriksGambar2):
         for j in range(len(matriksGambar1[0])):
             matriksGambar1[i][j] -= matriksGambar2[i][j]
     return matriksGambar1
+
+def KaliMatriks (matriksGambar1, matriksGambar2):
+    matrikshasil = np.dot(matriksGambar1, matriksGambar2)
+    return matrikshasil
