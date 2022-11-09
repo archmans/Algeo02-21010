7725d39fe65bfc2e106bbc284abfa205f0c3347a
agshaathalla <agshaathalla@gmail.com>
2022-11-09 20:57:16 +0700

    Add pengurangan matriks

--- src/operation.py
+++ src/operation.py
@@ -9,3 +9,8 @@ def JumlahMatriks (matriksGambar1, matriksGambar2):
             matriksGambar1[i][j] += matriksGambar2[i][j]
     return matriksGambar1
 
+def KurangMatriks (matriksGambar1, matriksGambar2):
+    for i in range(len(matriksGambar1)):
+        for j in range(len(matriksGambar1[0])):
+            matriksGambar1[i][j] -= matriksGambar2[i][j]
+    return matriksGambar1
