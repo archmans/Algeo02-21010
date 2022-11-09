da52b86be477842a15c2ecd2cf7f0f6f0ea6a275
agshaathalla <agshaathalla@gmail.com>
2022-11-09 21:39:45 +0700

    add procedure print matrix and driver

--- src/operation.py
+++ src/operation.py
@@ -22,3 +22,31 @@ def KaliMatriks (matriksGambar1, matriksGambar2):
 def TransposeMatriks (matriksGambar):
     matrikshasil = np.transpose(matriksGambar)
     return matrikshasil
+
+def printMatriks (matriksGambar):
+    for i in range(len(matriksGambar)):
+        for j in range(len(matriksGambar[0])):
+            print(matriksGambar[i][j], end=" ")
+        print()
+
+#Driver
+satu = [[0 for i in range(3)] for j in range(3)]
+dua = [[0 for i in range(3)] for j in range(3)]
+
+for i in range(3):
+    for j in range(3):
+        satu[i][j] = i+j
+        dua[i][j] = i-j
+
+print("\nMatriks 1")
+printMatriks(satu)
+print("\nMatriks 2")
+printMatriks(dua)
+print("\njumlah")
+printMatriks(JumlahMatriks(satu, dua))
+print("\nkurang")
+printMatriks(KurangMatriks(satu, dua))
+print("\nkali")
+printMatriks(KaliMatriks(satu, dua))
+print("\ntranspose")
+printMatriks(TransposeMatriks(satu))
