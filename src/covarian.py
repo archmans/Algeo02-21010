6d656f0c2ec38213e384b813f29a536ef2b5062a
Muhamad Salman Hakim Alfarisi <13521010@std.stei.itb.ac.id>
2022-11-16 21:01:52 +0700

    gui

--- src/covarian.py
+++ src/covarian.py
@@ -1,8 +1,14 @@
 import numpy as np
 from operation import *
+
 matrix = np.array([[1, 0, 0, 0, 1, 0], [1, 1, 0, 0, 0, 0], [0, 0, 1, 1, 0, 1]])
 def matrix_covarian(matrix):
-  return KaliMatriks(matrix, TransposeMatriks(matrix))
+  selisih = matrix
+  con_cov = selisih[0]
+  for i in range(1, len(selisih-1)):
+    con_cov = concat((con_cov, selisih[i]))
+  M = con_cov
+  return KaliMatriks(M, TransposeMatriks(M))
 
 cov = matrix_covarian(matrix)
 printMatriks(cov)
