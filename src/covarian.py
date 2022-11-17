82dfaae06153afa9f4bad47cc342f843d450d7d1
agshaathalla <agshaathalla@gmail.com>
2022-11-18 03:25:41 +0700

    revisi covarian

--- src/covarian.py
+++ src/covarian.py
@@ -3,12 +3,7 @@ from operation import *
 
 # matrix = np.array([[1, 0, 0, 0, 1, 0], [1, 1, 0, 0, 0, 0], [0, 0, 1, 1, 0, 1]])
 def matrix_covarian(matrix):
-  selisih = matrix
-  con_cov = selisih
-  # for i in range(1, len(selisih-1)):
-  #   con_cov = concat((con_cov, selisih[i]))
-  M = con_cov
-  return KaliMatriks(M, TransposeMatriks(M))
+  return np.cov(matrix)
 
 # cov = matrix_covarian(matrix)
 # printMatriks(cov)
