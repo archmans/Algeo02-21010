214f0269e467682c916ff2a5d023575fcf879061
agshaathalla <agshaathalla@gmail.com>
2022-11-22 20:07:22 +0700

    edit Matrix Covarian

--- src/covarian.py
+++ src/covarian.py
@@ -3,7 +3,8 @@ from operation import *
 
 # matrix = np.array([[1, 0, 0, 0, 1, 0], [1, 1, 0, 0, 0, 0], [0, 0, 1, 1, 0, 1]])
 def matrix_covarian(matrix):
-  return np.cov(matrix)
+  matrixT = np.transpose(matrix)
+  return matrix @ matrixT
 
 # cov = matrix_covarian(matrix)
 # printMatriks(cov)
